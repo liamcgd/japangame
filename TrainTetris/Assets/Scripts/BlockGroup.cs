3dde8689c07c601c711d6e0c6d3a87dfc3bf51f0
liamcgd <liam_cg_dunstan@yahoo.com.au>
2019-11-28 16:37:19 +0900

    fix click and drag

--- TrainTetris/Assets/Scripts/BlockGroup.cs
+++ TrainTetris/Assets/Scripts/BlockGroup.cs
@@ -6,29 +6,21 @@ public class BlockGroup : MonoBehaviour
 {
     private Vector3 screenPoint;
     private Vector3 offset;
-    [SerializeField] private bool clicked;
-
-    private void Start()
-    {
-        Debug.Log("Help");
-    }
 
     void OnMouseDown()
     {
-        clicked = true;
         screenPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
-        offset = Input.mousePosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1));
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
     }
 
     void OnMouseDrag()
     {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
         Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
         transform.position = curPosition;
     }
 
     private void OnMouseUp()
     {
-        clicked = false;
     }
 }
