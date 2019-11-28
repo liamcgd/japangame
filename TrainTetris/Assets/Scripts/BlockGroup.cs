c0966ada418cfe2e401df9098484c53c69055b61
liamcgd <liam_cg_dunstan@yahoo.com.au>
2019-11-28 16:21:59 +0900

    add project

---
+++ TrainTetris/Assets/Scripts/BlockGroup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockGroup : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    [SerializeField] private bool clicked;
+
+    private void Start()
+    {
+        Debug.Log("Help");
+    }
+
+    void OnMouseDown()
+    {
+        clicked = true;
+        screenPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        offset = Input.mousePosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -1);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    private void OnMouseUp()
+    {
+        clicked = false;
+    }
+}
---
+++ TrainTetris/Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] groups;
+
+    private void Start()
+    {
+        // Spawn initial Group
+        //SpawnNext();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            SpawnNext();
+        }
+    }
+
+    public void SpawnNext()
+    {
+        // Random Index
+        int i = Random.Range(0, groups.Length);
+
+        // Spawn Group at current Position
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+    }
+}
